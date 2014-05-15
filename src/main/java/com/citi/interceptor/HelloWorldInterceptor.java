7dc5cf9bc9ba2ba5440b1c7a8ce64e1ec4d7ee98
LarryChen1990 <larrychen1990@163.com>
2014-05-15 18:42:27 +0800

    c

---
+++ src/main/java/com/citi/interceptor/HelloWorldInterceptor.java
@@ -0,0 +1,32 @@
+package com.citi.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class HelloWorldInterceptor implements HandlerInterceptor  {
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+
+        System.out.println("Pre-handle");
+
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        System.out.println("Post-handle");
+    }
+
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+        System.out.println("After completion handle");
+        String result = "result simida";
+        response.getWriter().println(result);
+        System.out.println(result);
+    }
+}
