a0638775b8915ec58dc8a7e6a056ea0aac1f2e5a
LarryChen1990 <larrychen1990@163.com>
2014-05-15 17:16:13 +0800

    f

--- src/main/java/com/citi/listener/SessionListener.java
+++ src/main/java/com/citi/listener/SessionListener.java
@@ -9,26 +9,24 @@ import javax.servlet.http.HttpSessionListener;
 public class SessionListener implements HttpSessionListener {
 
 	private String sessionId;
+
 	public void sessionCreated(HttpSessionEvent evt) {
-		// TODO Auto-generated method stub
 		System.out.println("session created");
-		Date date=new Date();
+		Date date = new Date();
 		HttpSession session = evt.getSession();
 		session.setAttribute("loginDate", date);
-		System.out.println("set date to session "+date);
-		sessionId = evt.getSession().getId();
+		System.out.println("set date to session:" + date);
+		sessionId = session.getId();
 		System.out.println(sessionId);
-		
 	}
 
 	public void sessionDestroyed(HttpSessionEvent evt) {
-		// TODO Auto-generated method stub
 		HttpSession session = evt.getSession();
 		Date date = (Date) session.getAttribute("loginDate");
-		
-		System.out.println("get date from session "+date);
+		System.out.println(session.getSessionContext());
+		System.out.println(session.getId());
+		System.out.println("get date from session:" + date);
 		System.out.println("session destroyed");
-		
 	}
 
 }
