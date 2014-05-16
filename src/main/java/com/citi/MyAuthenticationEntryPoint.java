f122bae7ab938973517d52d253e24d3ca75b7dec
LarryChen1990 <larrychen1990@163.com>
2014-05-16 17:49:06 +0800

    fg

--- src/main/java/com/citi/MyAuthenticationEntryPoint.java
+++ src/main/java/com/citi/MyAuthenticationEntryPoint.java
@@ -7,16 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
-public class MyAuthenticationEntryPoint implements AuthenticationEntryPoint {
+public class MyAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
 
 	public void commence(HttpServletRequest request,
 			HttpServletResponse response, AuthenticationException authException)
 			throws IOException, ServletException {
 		// TODO Auto-generated method stub
+		super.commence(request, response, authException);
 		System.out.println("in MyAuthenticationEntryPoint");
 		
 	}
+	
+	
 
 }
