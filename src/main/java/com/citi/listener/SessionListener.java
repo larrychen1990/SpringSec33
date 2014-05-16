d0f9958b943317b9c1b1a5a57c028e146fc4c505
LarryChen1990 <larrychen1990@163.com>
2014-05-16 10:07:52 +0800

    d

--- src/main/java/com/citi/listener/SessionListener.java
+++ src/main/java/com/citi/listener/SessionListener.java
@@ -1,31 +1,16 @@
 package com.citi.listener;
 
-import java.util.Date;
-
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 public class SessionListener implements HttpSessionListener {
 
-	private String sessionId;
 
 	public void sessionCreated(HttpSessionEvent evt) {
 		System.out.println("session created");
-		Date date = new Date();
-		HttpSession session = evt.getSession();
-		session.setAttribute("loginDate", date);
-		System.out.println("set date to session:" + date);
-		sessionId = session.getId();
-		System.out.println(sessionId);
 	}
 
 	public void sessionDestroyed(HttpSessionEvent evt) {
-		HttpSession session = evt.getSession();
-		Date date = (Date) session.getAttribute("loginDate");
-		System.out.println(session.getSessionContext());
-		System.out.println(session.getId());
-		System.out.println("get date from session:" + date);
 		System.out.println("session destroyed");
 	}
 
--- src/main/java/com/citi/login/UserLoginFilter.java
+++ src/main/java/com/citi/login/UserLoginFilter.java
@@ -3,7 +3,6 @@ package com.citi.login;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -19,11 +18,18 @@ public class UserLoginFilter extends UsernamePasswordAuthenticationFilter {
     private String usernameParameter = SPRING_SECURITY_FORM_USERNAME_KEY;
     private String passwordParameter = SPRING_SECURITY_FORM_PASSWORD_KEY;
     
+    
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-
-    	System.out.println("in UserLoginFilter attemptAuthentication"+request+"::::"+response);
+    		
+    	System.out.println(new StringBuilder("in UserLoginFilter attemptAuthentication ")
+    							.append("\n request=")
+    							.append(request)
+    							.append("\n response=")
+    							.append(response)
+    							.append("\n"));
         String username = obtainUsername(request);
         String password = obtainPassword(request);
+        System.out.println(String.format("in UserLoginFilter attemptAuthentication \n username=%s password=%s \n", username,password));
 
         if (username == null) {
             username = "";
@@ -44,23 +50,26 @@ public class UserLoginFilter extends UsernamePasswordAuthenticationFilter {
     }
     
     protected String obtainPassword(HttpServletRequest request) {
-    	System.out.println("in UserLoginFilter obtainPassword"+request);
+    	System.out.println(new StringBuilder("in UserLoginFilter obtainPassword ")
+    	.append("\n request=")
+    	.append(request)
+    	.append("\n"));
         return request.getParameter(passwordParameter);
     }
     
     protected String obtainUsername(HttpServletRequest request) {
-    	System.out.println("in UserLoginFilter obtainUsername"+request);
+    	System.out.println(String.format("in UserLoginFilter obtainUsername \n request=%s \n", request));
         return request.getParameter(usernameParameter);
     }
     
     public void setUsernameParameter(String usernameParameter) {
-    	System.out.println("in UserLoginFilter setUsernameParameter"+usernameParameter);
+    	System.out.println(String.format("in UserLoginFilter setUsernameParameter \n usernameParameter=%s \n", usernameParameter));
         Assert.hasText(usernameParameter, "Username parameter must not be empty or null");
         this.usernameParameter = usernameParameter;
     }
 
     public void setPasswordParameter(String passwordParameter) {
-    	System.out.println("in UserLoginFilter setPasswordParameter:"+passwordParameter);
+    	System.out.println(String.format("in UserLoginFilter setPasswordParameter \n passwordParameter=%s \n", passwordParameter));
         Assert.hasText(passwordParameter, "Password parameter must not be empty or null");
         this.passwordParameter = passwordParameter;
     }
